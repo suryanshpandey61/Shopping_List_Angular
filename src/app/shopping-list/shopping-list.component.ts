a09fc5cc01d0ad849a7790ed6d0a44da8ed4e795
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-06-23 11:43:49 +0530

    ingrdients added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component } from "@angular/core";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
-
+import { Ingredient } from "../shared/ingredient.model";
 @Component({
     selector:'app-shopping-list',
     templateUrl:'./shopping-list.component.html',
     imports:[ShoppingEditComponent]
 })
 export class ShoppingListComponent{
-
+    ingredients:Ingredient[] = [
+        new Ingredient('Test ingedient',10),
+        new Ingredient('Paneer Masala',500),
+        new Ingredient('Musroom Masala',70),
+        new Ingredient('Garam Masala',1000)
+    ];
 }
